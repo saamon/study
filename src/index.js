8ab625640d4596594e3d65643d357083334ed52f
KENTO SUZUKI <81011337+saamon@users.noreply.github.com>
2022-05-26 22:37:01 +0900

    勉強 勉強

--- src/index.js
+++ src/index.js
@@ -62,24 +62,89 @@
 //  return str;
 // }
 
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
 
-//アロー関数 (=> 矢印がアローの由来)
-// const func2 = (str) => str;
+// //アロー関数 (=> 矢印がアローの由来)
+// // const func2 = (str) => str;
 
-const func2 = (str) => {
-  return str;
-};
+// const func2 = (str) => {
+//   return str;
+// };
 
-console.log(func2("fanc2です"));
+// console.log(func2("fanc2です"));
 
-//２つの足し算(アロー関数で書く)
+// //２つの足し算(アロー関数で書く)
 
-const add = (num1, num2) => {
-  return num1 + num2;
-};
+// const add = (num1, num2) => {
+//   return num1 + num2;
+// };
 
-console.log(add(10, 20));
+// console.log(add(10, 20));
+
+/**分割代入 */
+// const myProfile = {
+//   name: "Kento",
+//   age: 20
+// };
+
+// // const message1 = `名前は${myprofile.name}です。年齢は${myProfile.age}です`;
+
+// // console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${myProfile.name}です。年齢は${myProfile.age}です`;
+// console.log(message2);
+
+// const myProfile = ['Kento',20];
+
+// const message3 = `名前は${myProfile[0]}で年齢は${myProfile[1]}です`;
+// console.log(message3);
+
+// const [name ,age] = myProfile;
+// const message4 =`名前は${name}です。年齢は${age}です`
+// console.log(message4);
+
+/**デフォルト値,引数など */
+
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`)
+// // console.log(sayHello("Kento"));
+// sayHello("");
+
+/**スプレッド構文 */
+
+//配列の展開
+
+// const arr1 =[1,2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1 ,num2) => console.log(num1+num2);
+// sumFunc(arr1[0],arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2 =[1,2,3,4,5];
+// const [num1,num2,...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピーや結合
+// const arr4 =[10,20];
+// const arr5 =[30,40];
+
+// const arr6 =[...arr4];
+// arr6[0] =100;
+// console.log(arr6);
+// console.log(arr4);
+
+// const arr7 = [...arr4 , ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// console.log(arr8);
+// arr8[0] =100;
+// console.log(arr4);
